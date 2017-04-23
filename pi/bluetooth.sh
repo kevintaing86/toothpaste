bbb236959b691b989ae88e1369161f49e11969cc
Lee <lee@leesander.com>
2017-04-23 03:07:32 -0500

    fix ble beacon script

--- pi/bluetooth.sh
+++ pi/bluetooth.sh
@@ -1,5 +1,5 @@
-$ sudo hciconfig hci0 up
+sudo hciconfig hci0 up
 
-$ sudo hciconfig hci0 leadv 3
+sudo hciconfig hci0 leadv 3
 
-$ sudo hcitool -i hci0 cmd 0x08 0x0008 1b 02 01 06 03 03 aa fe 13 16 aa fe 10 00 03 67 6f 6f 2e 67 6c 2f 47 38 65 6c 6a 44 00 00 00 00
+sudo hcitool -i hci0 cmd 0x08 0x0008 1b 02 01 06 03 03 aa fe 13 16 aa fe 10 00 03 67 6f 6f 2e 67 6c 2f 47 38 65 6c 6a 44 00 00 00 00
